0ab4c34cae68f65217bf7804cbff0a57921c96f1
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-03-01 15:17:23 -0300

    Adicionando rotas pra login e logout

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group([
+    'namespace' => 'App\Http\Controllers',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
 });
+
