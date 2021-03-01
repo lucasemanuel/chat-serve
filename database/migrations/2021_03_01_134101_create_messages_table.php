bb649eaa0d98429751b795c0ac95c11ff7024134
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-03-01 10:56:34 -0300

    Criando migrações

---
+++ database/migrations/2021_03_01_134101_create_messages_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->text('body');
+            $table->unsignedBigInteger('source_id');
+            $table->unsignedBigInteger('destination_id');
+            $table->timestamps();
+
+            $table->foreign('source_id')->references('id')->on('users');
+            $table->foreign('destination_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
