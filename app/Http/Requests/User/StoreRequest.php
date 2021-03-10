89af2b1a18194ad994a16e05267031db397a290d
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-03-10 10:32:29 -0300

    Somente letras e números no username

--- app/Http/Requests/User/StoreRequest.php
+++ app/Http/Requests/User/StoreRequest.php
@@ -14,8 +14,15 @@ class StoreRequest extends FormRequest
     public function rules()
     {
         return [
-            'username' => 'required|unique:users|min:6|max:32',
+            'username' => 'required|regex:/^[0-9A-Za-z]+$/u|unique:users|min:6|max:16',
             'password' => 'required|confirmed|min:6|max:32',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'username.regex' => 'Apenas letras ou números no campo :attribute',
         ];
     }
 }
