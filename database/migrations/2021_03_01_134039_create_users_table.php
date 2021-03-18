5274e62fbb41fc0374cb4eb5ab2aa33b44e42232
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-03-18 11:08:35 -0300

    Colocando username como único

--- database/migrations/2021_03_01_134039_create_users_table.php
+++ database/migrations/2021_03_01_134039_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username', 32);
+            $table->string('username', 32)->unique();
             $table->string('password');
             $table->timestamps();
         });
