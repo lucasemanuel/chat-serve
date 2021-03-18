c572568cf5745dff2fe5ba606037c024f91130b5
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-03-18 11:08:09 -0300

    Adicionando factory para criar usuários

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Support\Facades\Hash;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable implements JWTSubject
 {
+    use HasFactory;
+
     protected $fillable = [
         'username',
         'password',
